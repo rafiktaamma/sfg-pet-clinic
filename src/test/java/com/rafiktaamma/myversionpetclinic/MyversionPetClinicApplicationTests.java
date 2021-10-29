2a75164a6420b6fd79e9b368113ff66e62b13ad7
rafik <b.taamma@esi-sba.dz>
2021-10-29 17:02:09 +0200

    Initial project add

---
+++ src/test/java/com/rafiktaamma/myversionpetclinic/MyversionPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafiktaamma.myversionpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyversionPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
